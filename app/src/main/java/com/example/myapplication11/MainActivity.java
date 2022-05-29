dfa20e0257d73b8f6ea8f51d629e4f5f8de2eeba
kinza <bsef19a515@pucit.edu.pk>
2022-05-29 20:00:17 +0500

    working added

--- app/src/main/java/com/example/myapplication11/MainActivity.java
+++ app/src/main/java/com/example/myapplication11/MainActivity.java
@@ -3,12 +3,51 @@ package com.example.myapplication11;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayList<String> GroceryList = new ArrayList<>();
+    ListView listView;
+    Button btn;
+    EditText txt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        GroceryList.add("Apple");
+//        GroceryList.add("Banana");
+//        GroceryList.add("Desi Ghee");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,
+                GroceryList);
+        listView = findViewById(R.id.listview);
+        listView.setAdapter(arrayAdapter);
+
+        txt=findViewById(R.id.textView2);
+
+        btn=findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                GroceryList.add(txt.getText().toString());
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+
+
+
+
     }
+
 }
